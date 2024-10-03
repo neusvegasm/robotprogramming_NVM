0c26b260c327b8086216595e4e8fbbae94eab546
Neus Vegas Morales <neusvegasmorales@icloud.com>
2024-10-03 20:10:54 +0200

    CMake and executable for hello world created

---
+++ source/rp_01_compilers_and_build_systems/01_building_a_cpp_program/build/CMakeFiles/helloworld_program.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloworld_program.dir/hello_world.cpp.o"
+  "CMakeFiles/helloworld_program.dir/hello_world.cpp.o.d"
+  "helloworld_program"
+  "helloworld_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloworld_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
