b7e353c93fc9ba2388faa24d798faafbbcdcaf70
Neus Vegas Morales <neusvegasmorales@icloud.com>
2024-10-07 20:15:13 +0200

    Exercise 1 completed

---
+++ exercises/rp_01_compilers_and_build_systems/build/executables/CMakeFiles/mini_core.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mini_core.dir/many_object_in_stack_example.cpp.o"
+  "CMakeFiles/mini_core.dir/many_object_in_stack_example.cpp.o.d"
+  "mini_core"
+  "mini_core.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mini_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
